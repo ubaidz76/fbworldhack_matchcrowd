4a896d18b3b0030fa955ef1985b3ff52551765c6
itz <alit.mahendra@gmail.com>
2012-09-13 16:16:30 +0700

    login controller

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -2,6 +2,8 @@
 
 class Welcome extends CI_Controller {
 
+    function __construct() {
+        parent::__construct();
         $this->load->library('facebook', array(
             'appId' => $this->config->item('facebook_application_id'),
             'secret' => $this->config->item('facebook_secret_key'),
@@ -11,6 +13,23 @@ class Welcome extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('welcome_message');
+        $data['facebook_uid'] = NULL;
+        $data['facebook_token'] = NULL;
+
+        $user = $this->facebook->getUser();
+        $data['facebook_uid'] = $user;
+        $data['facebook_token'] = $this->facebook->getAccessToken();
+
+        if ($data['facebook_uid']) {
+            $user_profile = $this->facebook->api('/me');
+            $data['facebook_name'] = $user_profile['name'];
+
+            $this->session->set_userdata($data);
+
+            echo 'aaaaaaaaaaaaaaaaaaaaaa';
+        } else {
+            $url = $this->facebook->getLoginUrl(array('cancel_url' => site_url(), 'redirect_uri' => site_url('home/facebookonly/'), 'scope' => $this->config->item('facebook_perms')));
+            redirect($url);
+        }
 	}
 }
